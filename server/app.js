88c6311737d517a95cb4dab1035d749bfedac0e9
nikohuber <143717219+nikohuber@users.noreply.github.com>
2024-11-13 16:09:28 -0500

    npm tests

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ const redisClient = redis.createClient({
   url: process.env.REDISCLOUD_URL,
 });
 
-redisClient.on('error', err => console.log('Redis Client Error', err));
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
 redisClient.connect().then(() => {
   const app = express();
@@ -38,7 +38,7 @@ redisClient.connect().then(() => {
   app.use(compression());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  
+
   app.use(session({
     key: 'sessionid',
     store: new RedisStore({
@@ -48,13 +48,13 @@ redisClient.connect().then(() => {
     resave: false,
     saveUninitialized: false,
   }));
-  
+
   app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
   app.set('view engine', 'handlebars');
   app.set('views', `${__dirname}/../views`);
-  
+
   router(app);
-  
+
   app.listen(port, (err) => {
     if (err) { throw err; }
     console.log(`Listening on port ${port}`);
